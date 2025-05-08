678615e1cc87f76e700e8163466c4f200ffb2502
c-rothschild <158000738+c-rothschild@users.noreply.github.com>
2025-05-07 21:45:59 -0600

    created list of nodes for each route

--- map2graph/busmap.py
+++ map2graph/busmap.py
@@ -5,18 +5,21 @@ import osmnx as ox
 import json
 import os
 from dotenv import load_dotenv
+import asyncio
+import aiohttp
+from tqdm import tqdm
 
 load_dotenv()
-
 MTA_API_KEY = os.getenv('MTA_API_KEY')
 GRAPHML_PATH = os.getenv('GRAPHML_PATH')
 ROUTE_LISTS_PATH = os.getenv('ROUTE_LISTS_PATH')
 BUS_STOP_INFO_PATH = os.getenv('BUS_STOP_INFO_PATH')
 
+
 class BusMap:
 
     def __init__(self):
-        self.graph = ox.io.load_graphml(filepath=GRAPHML_PATH)
+        self.G = ox.io.load_graphml(GRAPHML_PATH)
 
         with open(ROUTE_LISTS_PATH) as json_file:
             self.route_lists = json.load(json_file)
@@ -25,29 +28,57 @@ class BusMap:
             self.bus_stop_info = json.load(json_file)
 
 
-    def load_routes(self, route_lists):
+    def load_routes(self):
         """
-        store items in route list as dictionary of multidigraphs
+        adds route name(s) to bus stop dict
         """
 
         self.routes = {}
 
         #iterate through each route in route_lists
+        for route_name in self.route_lists.keys():
+            for stop in self.route_lists[route_name]:
+                self.bus_stop_info[stop]['diroutes'] = self.bus_stop_info[stop].get('diroutes', []) + [route_name]
+                self.bus_stop_info[stop]['diroutes'] = list(set(self.bus_stop_info[stop]['diroutes'] ))
+
+        filename = BUS_STOP_INFO_PATH
+        with open(filename, 'w') as file:
+            json.dump(self.bus_stop_info, file, indent=4)
+
+
+    def set_route_subgraphs(self, file_path=False):
+        self.route_graphs = {}
+        for name, route_list in tqdm(self.route_lists.items(), desc="Processing dictionary"):
+            self.route_graphs[name] = self.get_route_subgraph(route_list) 
+        if file_path:
+            with open(file_path, 'w') as file:
+                json.dump(self.route_graphs, file, indent=4)
+
+
+    def get_route_subgraph(self, route_list):
+        osmid_list = list(map(lambda x: self.bus_stop_info[x]['osmid'], route_list))
+        full_route = []
+        for i in range(1, len(osmid_list)):
+            path = ox.routing.shortest_path(self.G, osmid_list[i - 1], osmid_list[i])
+
+            if not path:
+                print(f'error: {(osmid_list[i - 1], osmid_list[i])}')
+            else:
+                full_route.extend(path[1:])
+        return full_route
+
+
+
+    def plot_route(self, route_name):
+        route_list = self.route_lists[route_name]
+        osmid_list = list(map(lambda x: self.bus_stop_info[x]['osmid'], route_list))
+        print(osmid_list)
+        ox.plot.plot_graph_route(self.G, osmid_list)
+            
+
+
+if __name__ == "__main__":
+    myMap = BusMap()
+    myMap.set_route_subgraphs(file_path='map2graph/graph_data/route_node_lists.json')
+
     
-    def load_route(self, route_list, route_name): 
-        """
-        loads an individual route to data
-        """
-        for stop in route_list:
-            # add route name to stop info
-            self.bus_stop_info[stop]['diroutes'] = self.bus_stop_info[stop].get('diroutes', []) + [route_name]
-            print(f"{route_name} added to stop {stop}")
-
-
-
-myMap = BusMap()
-
-print(myMap.routes)
-
-
-
